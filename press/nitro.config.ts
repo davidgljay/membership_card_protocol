2dd913a70740954b7826c38657fe04ec9610f028
David Jay <davidgljay@gmail.com>
2026-06-29 20:14:32 -0700

    Updating press to better utilize Nitro.

--- press/nitro.config.ts
+++ press/nitro.config.ts
@@ -1,6 +1,10 @@
 import { defineNitroConfig } from 'nitropack';
 
 export default defineNitroConfig({
+  // Default deployment target is Cloudflare Workers. Operators may build
+  // against an alternate preset via NITRO_PRESET (see package.json scripts
+  // build:lambda / build:node) without changing application code.
+  preset: 'cloudflare-module',
   srcDir: 'server',
   routeRules: {},
   scheduledTasks: {
